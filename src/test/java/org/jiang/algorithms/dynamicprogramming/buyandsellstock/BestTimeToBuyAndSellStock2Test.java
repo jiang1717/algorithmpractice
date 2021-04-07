8ea3894cca0453e918f863fde536c22a6f5126d6
jiang1717 <jiang1717@aliyun.com>
2021-04-07 09:31:12 +0800

    买卖股票的最佳时机II

--- src/test/java/org/jiang/algorithms/dynamicprogramming/buyandsellstock/BestTimeToBuyAndSellStock2Test.java
+++ src/test/java/org/jiang/algorithms/dynamicprogramming/buyandsellstock/BestTimeToBuyAndSellStock2Test.java
@@ -2,22 +2,26 @@ package org.jiang.algorithms.dynamicprogramming.buyandsellstock;
 
 import org.junit.Test;
 
+import static org.junit.Assert.*;
 
-public class BestTimeToBuyAndSellStockTest {
+public class BestTimeToBuyAndSellStock2Test {
 
     int[] prices;
     int result;
-    BestTimeToBuyAndSellStock btbss = new BestTimeToBuyAndSellStock();
+    BestTimeToBuyAndSellStock2 btbss = new BestTimeToBuyAndSellStock2();
 
     @Test
     public void maxProfit() {
         prices = new int[]{7, 1, 5, 3, 6, 4};
         result = btbss.maxProfit(prices);
-        System.out.println(result);//5
+        System.out.println(result);//7
+
+        prices = new int[]{1, 2, 3, 4, 5};
+        result = btbss.maxProfit(prices);
+        System.out.println(result);//4
 
         prices = new int[]{7, 6, 4, 3, 1};
         result = btbss.maxProfit(prices);
         System.out.println(result);//0
-
     }
 }
